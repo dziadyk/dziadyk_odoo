72ebceba2ebad41a8dc12b8e433bade00f6f033c
Dziadyk Volodymyr <dziadyk_v@ukr.net>
2022-10-27 21:33:38 +0300

    [IMP] hr_hospital: new features

--- hr_hospital/models/patient.py
+++ hr_hospital/models/patient.py
@@ -1,4 +1,4 @@
-from datetime import date
+import datetime
 from odoo import api, exceptions, fields, models, _
 
 
@@ -11,22 +11,32 @@ class Patient(models.Model):
     birthday = fields.Date(string='Date of Birth', required=True)
     age = fields.Integer(compute='_compute_age')
     passport = fields.Char()
-    personal_doctor_id = fields.Many2one(comodel_name='hr.hosp.doctor')
+    doctor_id = fields.Many2one(comodel_name='hr.hosp.doctor', string='Personal Doctor')
     emergency_contact_ids = fields.Many2many(comodel_name='hr.hosp.emergency.contact')
+
+    def write(self, vals):
+        if vals.get('doctor_id'):
+            doctor_history_dict = {
+                'patient_id': vals['doctor_id'],
+                'doctor_id': self.id,
+                'datetime': datetime.datetime.now()
+            }
+            self.env['hr.hosp.personal.doctor.history'].create(doctor_history_dict)
+        return super(Patient, self).write(vals)
 
     @api.depends('birthday')
     def _compute_age(self):
-        today = date.today()
+        today = datetime.date.today()
         for obj in self:
             if obj.birthday:
                 obj.age = obj.age = today.year - obj.birthday.year - (
-                            (today.month, today.day) < (obj.birthday.month, obj.birthday.day))
+                        (today.month, today.day) < (obj.birthday.month, obj.birthday.day))
             else:
                 obj.age = 0
 
     @api.constrains('birthday')
     def constrains_birthday(self):
-        today = date.today()
+        today = datetime.date.today()
         for obj in self:
             if obj.birthday > today:
                 raise exceptions.ValidationError(_('Birthday must be less than today'))
--- hr_hospital/models/personal_doctor_history.py
+++ hr_hospital/models/personal_doctor_history.py
@@ -6,6 +6,6 @@ class PersonalDoctorHistory(models.Model):
     _description = 'Personal doctor history'
 
     active = fields.Boolean(default=True)
-    patient_id = fields.Many2one(comodel_name='hr.hosp.patient', required=True)
-    doctor_id = fields.Many2one(comodel_name='hr.hosp.doctor', required=True)
-    date = fields.Datetime(required=True)
+    patient_id = fields.Many2one(comodel_name='hr.hosp.patient', ondelete='cascade', required=True)
+    doctor_id = fields.Many2one(comodel_name='hr.hosp.doctor', ondelete='cascade', required=True)
+    datetime = fields.Datetime(string='Date', required=True)
