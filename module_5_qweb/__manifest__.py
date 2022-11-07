4ec409e239259d6e61265fff11df545538e06ffb
Dziadyk Volodymyr <dziadyk_v@ukr.net>
2022-11-07 21:23:52 +0200

    [IMP] module_5_qweb: new features

---
+++ module_5_qweb/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Module 5. Reports and QWeb",
+    "description": """This module for learning Odoo Qweb.""",
+    "summary": "learning Odoo Qweb",
+    "website": "https://bjetpro.com",
+    "author": "BJet",
+    "category": "Education",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "web",
+    ],
+    "qweb": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/data.xml",
+        "report/models_reports_templates.xml",
+        "report/models_report.xml",
+        "views/models_views.xml",
+    ],
+    'images': [
+        'static/description/icon.png'
+    ]
+}
---
+++ module_5_qweb/models/models.py
@@ -0,0 +1,54 @@
+import logging
+
+from odoo import models, fields, _
+
+_logger = logging.getLogger(__name__)
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Book'
+
+    name = fields.Char(
+        string=_('Name'),
+        translate=True,
+        required=True, )
+    book_description = fields.Text(
+        string=_('Text'),
+        translate=True, )
+    author_id = fields.Many2one(
+        comodel_name='author.of.book',
+        string=_('Author'), index=True, ondelete='cascade')
+    additional_text = fields.Text(
+        string=_('Additional Text'),
+        translate=True, )
+
+    def set_confirm(self):
+        pass
+
+
+class AuthorOfBook(models.Model):
+    _name = 'author.of.book'
+    _description = 'Author of book'
+
+    name = fields.Char(
+        string=_('Name'),
+        translate=True,
+        required=True, )
+    last_name = fields.Char(
+        string=_('Last Name'),
+        translate=True, )
+    biography_text = fields.Text(
+        string=_('Text'),
+        translate=True, )
+    book_ids = fields.One2many(
+        comodel_name='library.book',
+        inverse_name='author_id',
+        string=_('Books'), )
+
+    def set_confirm(self):
+        pass
+
+    def get_full_name(self):
+        self.ensure_one()
+        return f'{self.name} {self.last_name}'
