71e2ce4c83769814ed4d14dca410af674f6f715d
dziadyk <dziadyk_v@ukr.net>
2022-11-16 12:17:28 +0200

    [IMP] school_lesson_6_2: new features

--- school_lesson_6_2/models/library_author.py
+++ school_lesson_6_2/models/library_author.py
@@ -6,8 +6,8 @@ class LibraryAuthor(models.Model):
     _name = 'library.author'
     _description = 'Library Authors'
 
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
+    first_name = fields.Char(required=True, translate=True)
+    last_name = fields.Char(required=True, translate=True)
     birth_date = fields.Date('Birthday')
     trainee_access = fields.Boolean(compute='_compute_trainee_access',)
 
